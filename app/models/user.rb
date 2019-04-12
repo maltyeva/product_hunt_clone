b4db7cbd2fc214af96d45507bd8b38d6049d5e7d
Maria <maria.altyeva@gmail.com>
2019-04-12 09:48:56 +0900

    prepare app for travis integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  def full_name
+    if first_name.nil?
+      last_name.capitalize
+    else
+      "#{first_name.capitalize} #{last_name.capitalize}"
+    end
+  end
+
 end
