2022fc123578b2f41db628f7514b1f1c155b2776
Maria <maria.altyeva@gmail.com>
2019-04-12 09:49:28 +0900

    broke test on purpose

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "full_name returns the capitalized first name and last name" do
     user = User.new(last_name: "lennon")
-    assert_equal "Lennon", user.full_name
+    assert_equal "John Lennon", user.full_name
   end
 end
